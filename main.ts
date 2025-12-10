a961631e4ca27d57b055738791ea04675fc90a01
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-12-10 12:52:43 -0500

    sends a message to another microbit if it is to close to an object

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matheus Quinzan
+ * Created on: Nov 2025
+ * This program says to a microbit if its too close to an object if it is less than 10 cm.
 */
 
-basic.showString('Hello, World!')
+// variable
+let distanceToAnObject: number = 0
+
+// setup
+radio.setGroup(77)
+basic.clearScreen()
+basic.showIcon(IconNames.Duck)
+
+while (true) {
+    // gets the distance from the sonar
+    distanceToAnObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showIcon(IconNames.Duck)
+
+    if (distanceToAnObject < 10) {
+        radio.sendString("Too Close")
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+}
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Duck)
+})
